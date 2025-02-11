df3437a5e0b3c249fb69b4ab9ffac517ad8bef5b
Peter Karkulik <peter.karkulik@gmail.com>
2025-02-11 14:52:09 +0100

    new mongoose schema

--- backend/models/skillSchema.js
+++ backend/models/skillSchema.js
@@ -1,6 +1,6 @@
 const mongoose = require('mongoose')
 
-const skillSchema = mongoose.Schema({
+const skillSchema = new mongoose.Schema({
     name: {
         type: String,
         required: true,
--- backend/models/userSchema.js
+++ backend/models/userSchema.js
@@ -2,7 +2,7 @@ const mongoose = require('mongoose')
 const roleEnum = require('./enums/roleEnum')
 const employmentTypeEnum = require('./enums/employmentTypeEnum')
 
-const userSchema = mongoose.Schema({
+const userSchema = new mongoose.Schema({
     username: {
         type: String,
         required: true,
