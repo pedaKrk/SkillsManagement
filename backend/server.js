171ff4ca9b69fe00407d0fb9c60484724ca02fb7
Peter Karkulik <peter.karkulik@gmail.com>
2025-02-12 00:53:45 +0100

    use routes in server.js

--- backend/server.js
+++ backend/server.js
@@ -1,12 +1,17 @@
 import express from 'express'
-import routes from './routes'
+import userRoutes from './routes/user.routes.js'
+import skillRoutes from './routes/skill.routes.js'
+import commentRoutes from './routes/comment.routes.js'
 import connectToDB from "./database/mongodb.js";
 
 const app = express()
 const PORT = 3000
 
 app.use(express.json())
-app.use('/api/v1', routes)
+
+app.use('/api/v1/users', userRoutes)
+app.use('/api/v1/skills', skillRoutes)
+app.use('/api/v1/comments', commentRoutes)
 
 app.listen(PORT, async () => {
   console.log(`Server is running on http://localhost:${PORT}`);
