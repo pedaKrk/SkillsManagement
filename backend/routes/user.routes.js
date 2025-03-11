aa38967ba08e76e660eb9af140ed39d6b3cdfe59
Peter Karkulik <peter.karkulik@gmail.com>
2025-03-11 21:11:50 +0100

    use auth only on required routes, add new change-password route

--- backend/routes/user.routes.js
+++ backend/routes/user.routes.js
@@ -1,24 +1,26 @@
 import express from 'express'
-const router = express.Router()
-import { getAllUsers, getUserById, createUser, updateUser, deleteUser } from '../controllers/user.controller.js'
+import {
+    getAllUsers,
+    getUserById,
+    createUser,
+    updateUser,
+    deleteUser,
+    changePassword
+} from '../controllers/user.controller.js'
 import { authenticateToken, authorizeRole } from '../middleware/auth.middleware.js'
+const router = express.Router()
 
-// Alle Routen sind geschützt
-router.use(authenticateToken)
 
-// Nur Admin darf alle User sehen
-router.get('/', authorizeRole(['Admin']), getAllUsers)
+router.get('/', authenticateToken, authorizeRole(['Admin']), getAllUsers)
 
-// User dürfen ihre eigenen Daten sehen
-router.get('/:id', getUserById)
+router.get('/:id', authenticateToken, getUserById)
 
-// Nur Admin darf User erstellen
-router.post('/', authorizeRole(['Admin']), createUser)
+router.post('/', authenticateToken, authorizeRole(['Admin']), createUser)
 
-// User können nur ihre eigenen Daten ändern, Admin kann alle ändern
-router.put('/:id', updateUser)
+router.put('/:id', authenticateToken, updateUser)
 
-// Nur Admin darf User löschen
-router.delete('/:id', authorizeRole(['Admin']), deleteUser)
+router.delete('/:id', authenticateToken, authorizeRole(['Admin']), deleteUser)
+
+router.post('/change-password', changePassword)
 
 export default router
