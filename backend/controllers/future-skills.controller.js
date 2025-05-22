95fcb7db2998ecdc5e47eabe6ac81cb5e13317bc
Peter Karkulik <peter.karkulik@gmail.com>
2025-05-23 01:00:56 +0200

    use repo and service for future skill

--- backend/controllers/future-skills.controller.js
+++ backend/controllers/future-skills.controller.js
@@ -1,8 +1,9 @@
 import Skill from '../models/skill.model.js'
-import FutureSkills from '../models/future-skill.model.js';
 import mongoose from 'mongoose';
 import skillLevelEnum from '../models/enums/skill.level.enum.js';
+import * as futureSkillService from '../services/future.skill.service';
 
+// ToDo: move to skill endpoint
 export const getAllSkillNames = async (req, res) => {
     try {
         const names = await Skill.find('name');
@@ -12,7 +13,7 @@ export const getAllSkillNames = async (req, res) => {
     }
 };
 
-
+// ToDo: move to skill endpoint
 export const getSkillLevels = (req, res) => {
     try {
         const levels = Object.values(skillLevelEnum);
@@ -22,65 +23,52 @@ export const getSkillLevels = (req, res) => {
     }
 };
 
-
-// GET all future skills
 export const getAllFutureSkills = async (req, res) => {
     try {
-        const skills = await FutureSkills.find()
-            .populate('lecturer_id', 'title firstName lastName')
-            .populate('skill_id', 'name');
+        const skills = await futureSkillService.getAllFutureSkills()
         res.status(200).json(skills);
     } catch (err) {
         res.status(500).json({ message: 'Error fetching skills', error: err });
     }
 };
 
-
-
-// POST create new skill
 export const createFutureSkill = async (req, res) => {
     try {
-        const newSkill = new FutureSkills(req.body);
-        await newSkill.save();
+        const data = req.body;
+        const newSkill = await futureSkillService.createFutureSkill(data);
         res.status(201).json(newSkill);
     } catch (err) {
         res.status(400).json({ message: 'Error creating skill', error: err });
     }
 };
 
-// PUT update skill by ID
 export const updateFutureSkill = async (req, res) => {
-    const { id } = req.params;
-    const updatedData = req.body;
-
-    if (!mongoose.Types.ObjectId.isValid(id)) {
-        return res.status(400).json({ message: 'Invalid skill ID' });
-    }
-
     try {
-        const updatedSkill = await FutureSkills.findByIdAndUpdate(id, updatedData, { new: true });
-        if (!updatedSkill) {
-            return res.status(404).json({ message: 'Skill not found' });
+        const { id } = req.params;
+        const updatedData = req.body;
+
+        if (!mongoose.Types.ObjectId.isValid(id)) {
+            return res.status(400).json({ message: 'Invalid skill ID' });
         }
+
+        const updatedSkill = await futureSkillService.updateFutureSkill(id, updatedData)
+
         res.status(200).json(updatedSkill);
     } catch (err) {
         res.status(500).json({ message: 'Error updating skill', error: err });
     }
 };
 
-// DELETE skill by ID ✅
 export const deleteFutureSkill = async (req, res) => {
-    const { id } = req.params;
-
-    if (!mongoose.Types.ObjectId.isValid(id)) {
-        return res.status(400).json({ message: 'Invalid skill ID' });
-    }
-
     try {
-        const deletedSkill = await FutureSkills.findByIdAndDelete(id);
-        if (!deletedSkill) {
-            return res.status(404).json({ message: 'Skill not found' });
+        const { id } = req.params;
+
+        if (!mongoose.Types.ObjectId.isValid(id)) {
+            return res.status(400).json({ message: 'Invalid skill ID' });
         }
+
+        await futureSkillService.deleteFutureSkill(id)
+
         res.status(200).json({ message: 'Skill deleted successfully' });
     } catch (err) {
         res.status(500).json({ message: 'Error deleting skill', error: err });
