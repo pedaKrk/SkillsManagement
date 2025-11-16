185c94ccbed873b88db77e1e8598eb23f435332e
Peter Karkulik <peter.karkulik@gmail.com>
2025-11-16 18:49:26 +0100

    add new methods

--- backend/repositories/future.skill.repository.js
+++ backend/repositories/future.skill.repository.js
@@ -1,15 +1,112 @@
 import FutureSkill from '../models/future.skill.model.js'
 
-export const findAllFutureSkills = () =>
-    FutureSkill.find()
-        .populate('lecturer_id', 'title firstName lastName')
-        .populate('skill_id', 'name')
+class FutureSkillRepository {
+    findAllFutureSkills = () =>
+        FutureSkill.find()
+            .populate('lecturer_id', 'title firstName lastName')
+            .populate('skill_id', 'name')
 
-export const createFutureSkill = (data) => new FutureSkill(data).save()
+    createFutureSkill = (data) => new FutureSkill(data).save()
 
-export const findFutureSkillById = (id) => FutureSkill.findById(id)
+    findFutureSkillById = (id) => FutureSkill.findById(id)
 
-export const updateFutureSkill = (id, data) =>
-    FutureSkill.findByIdAndUpdate(id, data, { new: true })
+    updateFutureSkill = (id, data) =>
+        FutureSkill.findByIdAndUpdate(id, data, {new: true})
 
-export const deleteFutureSkill = (id) => FutureSkill.findByIdAndDelete(id)
+    deleteFutureSkill = (id) => FutureSkill.findByIdAndDelete(id)
+
+    getFutureSkillLevelMatrix = () => {
+        return FutureSkill.aggregate([
+            {
+                $lookup: {
+                    from: 'skills',
+                    localField: 'skill_id',      // OLD FIELD NAME
+                    foreignField: '_id',
+                    as: 'skill'
+                }
+            },
+            {$unwind: '$skill'},
+            {
+                $group: {
+                    _id: {skill: '$skill.name', level: '$future_achievable_level'},   // OLD FIELD NAME
+                    count: {$sum: 1}
+                }
+            },
+            {
+                $group: {
+                    _id: '$_id.skill',
+                    levels: {
+                        $push: {
+                            level: '$_id.level',
+                            count: '$count'
+                        }
+                    }
+                }
+            },
+            {
+                $project: {
+                    name: '$_id',
+                    series: {
+                        $map: {
+                            input: '$levels',
+                            as: 'lvl',
+                            in: {
+                                name: '$$lvl.level',
+                                value: '$$lvl.count'
+                            }
+                        }
+                    }
+                }
+            }
+        ]);
+    }
+
+    getUserFutureSkillLevelMatrix = (userId) => {
+        return FutureSkill.aggregate([
+            {$match: {lecturer_id: userId}},
+            {
+                $lookup: {
+                    from: 'skills',
+                    localField: 'skill_id',      // OLD FIELD NAME
+                    foreignField: '_id',
+                    as: 'skill'
+                }
+            },
+            {$unwind: '$skill'},
+            {
+                $group: {
+                    _id: {skill: '$skill.name', level: '$future_achievable_level'},   // OLD FIELD NAME
+                    count: {$sum: 1}
+                }
+            },
+            {
+                $group: {
+                    _id: '$_id.skill',
+                    levels: {
+                        $push: {
+                            level: '$_id.level',
+                            count: '$count'
+                        }
+                    }
+                }
+            },
+            {
+                $project: {
+                    name: '$_id',
+                    series: {
+                        $map: {
+                            input: '$levels',
+                            as: 'lvl',
+                            in: {
+                                name: '$$lvl.level',
+                                value: '$$lvl.count'
+                            }
+                        }
+                    }
+                }
+            }
+        ]);
+    }
+}
+
+export const futureSkillRepository = new FutureSkillRepository();
