7f0eba6fd361949b1d2455c11c6ba9ad9360b84d
Peter Karkulik <peter.karkulik@gmail.com>
2025-12-18 22:53:46 +0100

    fix endpoints

--- frontend/src/app/core/services/dashboard.service.ts
+++ frontend/src/app/core/services/dashboard.service.ts
@@ -1,36 +1,65 @@
 import { Injectable } from '@angular/core';
 import { HttpClient } from '@angular/common/http';
-import { Observable } from 'rxjs';
+import {Observable, throwError} from 'rxjs';
 import {API_CONFIG} from '../config/api.config';
+import {catchError} from 'rxjs/operators';
 
 @Injectable({ providedIn: 'root' })
 export class DashboardService {
 
-  private apiUrl = 'http://localhost:3000/api/v1/dashboard';  // Adjust this path!
-
   constructor(private http: HttpClient) {}
 
   getSkillsLevelMatrix(): Observable<any> {
-    return this.http.get<any>(`${this.apiUrl}/skills-level-matrix`);
+    return this.http.get<any>(`${API_CONFIG.baseUrl}/${API_CONFIG.endpoints.dashboard.skillsLevelMatrix}`).pipe(
+      catchError(error => {
+        console.error('Error loading SkillsLevelMatrix:', error);
+        return throwError(() => new Error('Error getting SkillsLevelMatrix'));
+      })
+    );
   }
 
   getSkillsByLevel(): Observable<any> {
-    return this.http.get(`${this.apiUrl}/skills-by-level`);
+    return this.http.get(`${API_CONFIG.baseUrl}/${API_CONFIG.endpoints.dashboard.skillsByLevel}`).pipe(
+      catchError(error => {
+        console.error('Error loading Skills by Level:', error);
+        return throwError(() => new Error('Error getting Skills by Level'));
+      })
+    );
   }
 
   getSkillsPopularity(): Observable<any> {
-    return this.http.get(`${this.apiUrl}/skills-popularity`);
+    return this.http.get(`${API_CONFIG.baseUrl}/${API_CONFIG.endpoints.dashboard.skillsPopularity}`).pipe(
+      catchError(error => {
+        console.error('Error loading Skills popularity:', error);
+        return throwError(() => new Error('Error getting Skills popularity'));
+      })
+    );
   }
 
   getFieldsPopularity(): Observable<any> {
-    return this.http.get(`${this.apiUrl}/fields-popularity`);
+    return this.http.get(`${API_CONFIG.baseUrl}/${API_CONFIG.endpoints.dashboard.fieldsPopularity}`).pipe(
+      catchError(error => {
+        console.error('Error loading Fields popularity:', error);
+        return throwError(() => new Error('Error getting Fields popularity'));
+      })
+    );
   }
 
   getUserFutureSkillLevelMatrix(userId: string): Observable<any> {
-    return this.http.get(`${this.apiUrl}/user/${userId}/future-skills-level-matrix`);
+    return this.http.get(`${API_CONFIG.baseUrl}/${API_CONFIG.endpoints.dashboard.userFutureSkillLevelMatrix(userId)}`).pipe(
+      catchError(error => {
+        console.error(`Error loading FutureSkillsLevelMatrix for user ${userId}:`, error);
+        return throwError(() => new Error('Error getting FutureSkillsLevelMatrix'));
+      })
+    );
   }
 
   getUserSkillDistribution(userId: string): Observable<any> {
-    return this.http.get(`${this.apiUrl}/user/${userId}/skills/distribution`);
+    return this.http.get(`${API_CONFIG.baseUrl}/${API_CONFIG.endpoints.dashboard.userSkillsDistribution(userId)}`).pipe(
+      catchError(error => {
+        console.error(`Error loading SkillsDistribution for user ${userId}:`, error);
+        return throwError(() => new Error('Error getting SkillsDistribution'));
+      })
+    );
   }
 }
